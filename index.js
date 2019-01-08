bf6a07f3a70714cbec050cf9c9853bee12bb73e4
MichaelM3 <dagreekarican@aim.com>
2019-01-08 10:51:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+  let list = document.getElementById('grand-node').querySelectorAll('div')
+  return list[list.length - 1]
+}
+
+function increaseRankBy(n) {
+  const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
