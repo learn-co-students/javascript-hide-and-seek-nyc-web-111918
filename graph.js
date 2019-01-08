6e929e6b2379fe374ff3b12a365178419e8b6cfb
jpagnan0 <a.jpagnano@gmail.com>
2019-01-08 13:25:42 -0500

    Done.

---
+++ graph.js
@@ -0,0 +1,28 @@
+function Graph(v) {
+	this.vertices = v;
+	this.edges = 0;
+	this.adj = [];
+	for (let i = 0; i < this.vertices; ++i) {
+		this.adj[i] = [];
+		this.adj[i].push("");
+	}
+	this.addEdge = addEdge;
+	this.showGraph = showGraph;
+}
+
+function addEdge(v, w) {
+	this.adj[v].push(w);
+	this.adj[w].push(v);
+	this.edges++;
+}
+
+function showGraph() {
+	for (let i = 0; i < this.vertices; ++i) {
+		putstr(i + " -> ");
+		for (let j = 0; j < this.vertices; ++j) {
+			if (this.adj[i][j] != undefined)
+				putstr(this.adj[i][j] + ' ');
+		}
+		print();
+	}
+}
